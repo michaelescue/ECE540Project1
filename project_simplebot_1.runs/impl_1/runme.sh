38974b62d8edaaf77bee5d12841874172aecb74f
Aditi Sai Venkata Gundu <adigundu@ds.cecs.pdx.edu>
2019-10-16 20:17:43 -0700

    almost done

--- project_simplebot_1.runs/impl_1/runme.sh
+++ project_simplebot_1.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='N:/ECE540/Project1/project_simplebot_1.runs/impl_1'
+HD_PWD='N:/ECE540Project1-V1/project_simplebot_1.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log mfp_nexys4_ddr.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source mfp_nexys4_ddr.tcl -notrace
 
 
--- project_simplebot_1.runs/synth_1/runme.sh
+++ project_simplebot_1.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='N:/ECE540/Project1/project_simplebot_1.runs/synth_1'
+HD_PWD='N:/ECE540Project1-V1/project_simplebot_1.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
