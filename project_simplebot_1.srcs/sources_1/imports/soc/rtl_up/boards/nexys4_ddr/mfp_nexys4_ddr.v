50715ad4a81dd54a94ba6c5162ebf66ac7f4fa3e
michaelescue <mescue@pdx.edu>
2019-10-20 12:49:29 -0700

    working on verilog comments. Asm code is done.

--- project_simplebot_1.srcs/sources_1/imports/soc/rtl_up/boards/nexys4_ddr/mfp_nexys4_ddr.v
+++ project_simplebot_1.srcs/sources_1/imports/soc/rtl_up/boards/nexys4_ddr/mfp_nexys4_ddr.v
@@ -20,9 +20,9 @@ module mfp_nexys4_ddr(
                         input                   BTNU, BTND, BTNL, BTNC, BTNR, 
                         input  [`MFP_N_SW-1 :0] SW,
                         output [`MFP_N_LED-1:0] LED,
-                        //output [`MFP_N_SEG-1:0] SEG,
-                        output CA, CB, CC, CD, CE, CF, CG, DP,
-                        output [7:0] AN,
+                        output                  CA, CB, CC, CD, CE, CF, CG, DP, // Active low output into the cathode (and decimal point) pins- 
+                                                                                // for enabling individual segments. 
+                        output [ 7          :0] AN, // Active low output into the AN pins for enabling SSEG digits.
                         inout  [ 8          :1] JB,
                         input                   UART_TXD_IN);
 
@@ -30,9 +30,9 @@ module mfp_nexys4_ddr(
         
   wire clk_out; 
   wire tck_in, tck;
-  wire [7:0] io_wire;
-  wire [5:0] pbtn_db;
-  wire [`MFP_N_SW-1 :0] swtch_db;
+  wire [7:0] io_wire; // Used to bundle cathode signals into single bus from mfp_sys output IO_7SEG_N.
+  wire [5:0] pbtn_db; // Used for debounced signal of push button input from the debounce module.
+  wire [`MFP_N_SW-1 :0] swtch_db; // Used for debounced signal from switch input of the debounce module.
   
   
   assign io_wire = {DP,CA,CB,CC,CD,CE,CF,CG};       
@@ -41,10 +41,11 @@ module mfp_nexys4_ddr(
   IBUF IBUF1(.O(tck_in),.I(JB[4]));
   BUFG BUFG1(.O(tck), .I(tck_in));
   
+  // The debounce module accepts the pushbutton and switch inputs, then outputs debounced signals pbtn_db and switch_db for use in submodules.
   debounce debounce(.clk(clk_out), .pbtn_in({BTNU,BTND,BTNL,BTNC,BTNR,CPU_RESETN}), .switch_in(SW), .pbtn_db(pbtn_db), .swtch_db(swtch_db));
 
   mfp_sys mfp_sys(
-			        .SI_Reset_N(pbtn_db[0]),
+			        .SI_Reset_N(pbtn_db[0]), // Input is the debounced reset push button from the debounce module.
                     .SI_ClkIn(clk_out),
                     .HADDR(),
                     .HRDATA(),
@@ -58,11 +59,11 @@ module mfp_nexys4_ddr(
                     .EJ_TCK(tck),
                     .SI_ColdReset_N(JB[8]),
                     .EJ_DINT(1'b0),
-                    .IO_Switch(swtch_db),
-                    .IO_PB(pbtn_db[5:1]),
+                    .IO_Switch(swtch_db), // Input is the debounced switch signal from the debounce module.
+                    .IO_PB(pbtn_db[5:1]), // Input is the debounced push button signals for up, down, left, and right from the debounce module.
                     .IO_LED(LED),
-                    .IO_7SEGEN_N(AN),
-                    .IO_SEG_N(io_wire),
+                    .IO_7SEGEN_N(AN), // Output into the AN pins to enable SSEG digits which are set at the mfp_ahb_sevensegtimer submodule.
+                    .IO_SEG_N(io_wire), // Output into the net connecting all cathodes at the top level which are set at the mfp_ahb_sevensegtimer submodule.
                     .UART_RX(UART_TXD_IN));
           
 endmodule
--- project_simplebot_1.srcs/sources_1/imports/soc/rtl_up/system/mfp_sys.v
+++ project_simplebot_1.srcs/sources_1/imports/soc/rtl_up/system/mfp_sys.v
@@ -25,8 +25,8 @@ module mfp_sys(
                     input  [`MFP_N_SW-1 :0] IO_Switch,
                     input  [`MFP_N_PB-1 :0] IO_PB,
                     output [`MFP_N_LED-1:0] IO_LED,
-                    output [`MFP_N_SEG-1:0] IO_7SEGEN_N,
-                    output [`MFP_N_SEG-1:0] IO_SEG_N,
+                    output [`MFP_N_SEG-1:0] IO_7SEGEN_N, // Output for the top level SSEG digit enable pins.
+                    output [`MFP_N_SEG-1:0] IO_SEG_N, // Output for the top level segment and decimal point enable pins.
                     input                   UART_RX);
 
 
@@ -303,8 +303,8 @@ module mfp_sys(
         .IO_Switch              (   IO_Switch               ),
         .IO_PB                  (   IO_PB                   ),
         .IO_LED                 (   IO_LED                  ),
-        .IO_7SEGEN_N                 (   IO_7SEGEN_N                  ),
-        .IO_SEG_N                 (   IO_SEG_N                  ),
+        .IO_7SEGEN_N            (   IO_7SEGEN_N             ), // 
+        .IO_SEG_N               (   IO_SEG_N                ),
         .UART_RX                (   UART_RX                 ), 
         .MFP_Reset_serialload   (   MFP_Reset_serialload    )
     );
